9369fdf79f60de4fccff11e4eb0129313154c8a7
blueberryliao <7773237+blueberryliao@user.noreply.gitee.com>
2025-02-21 21:39:20 -0800

    update MD

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 // for all the routes management
 import router from "./routes/router.jsx";
-//
-import { IonIcon } from "@ionic/react";
-import { heartOutline, cartOutline } from "ionicons/icons";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
